3b64fa0fee979e3f59b6f630f383e3baa26b37e2
501 <501@academicos.cesde.edu.co>
2023-03-25 10:11:26 -0500

    Repaso

---
+++ baseDatos.js
@@ -0,0 +1,17 @@
+export let productos=[
+    {
+        nombre:"Camiseta1",
+        precio:80000,
+        foto:"https://firebasestorage.googleapis.com/v0/b/tiendasabadoasb.appspot.com/o/foto1.webp?alt=media&token=11d4e9b4-da6b-4ac4-856f-93c6d7ab273f"
+    },
+    {
+        nombre:"Camiseta2",
+        precio:85000,
+        foto:"https://firebasestorage.googleapis.com/v0/b/tiendasabadoasb.appspot.com/o/foto2.webp?alt=media&token=cb5086a6-b000-43d5-acb8-f3a172167ad9"
+    },
+    {
+        nombre:"Camiseta3",
+        precio:150000,
+        foto:"https://firebasestorage.googleapis.com/v0/b/tiendasabadoasb.appspot.com/o/foto3.webp?alt=media&token=fe664df8-856a-4ed9-80c1-f36b6e275f0f"
+    }
+]
